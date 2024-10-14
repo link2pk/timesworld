1e56e616b7eb4a60e4ed688a7e5d51b4c1c5f336
praveen.kumar <praveen.kumar@gmail.com>
2024-10-14 21:31:32 +0530

    add repo name

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { resolve } from 'path'
 export default {
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        emptyOutDir: true // Clean the output folder before building
     },
     server: {
         port: 8080
-    }
+    },
+    base: '/timesworld/', // Update with the correct repository name
 }
